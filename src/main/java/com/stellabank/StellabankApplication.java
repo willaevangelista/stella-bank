b98ae3fe048ebcf67e9dfb2ddecbe8c7afd13794
willa <willaevangelista.pro@gmail.com>
2026-04-19 09:48:47 -0300

    chore: initial project setup with Spring Initializr

---
+++ src/main/java/com/stellabank/StellabankApplication.java
@@ -0,0 +1,13 @@
+package com.stellabank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StellabankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StellabankApplication.class, args);
+	}
+
+}
